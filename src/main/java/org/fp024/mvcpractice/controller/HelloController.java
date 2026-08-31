d8060317a48a43562e51b60b92683d1ccc3cecfe
fp024 <aopenasus@naver.com>
2026-08-31 18:45:05 +0900

    Thymeleaf 페이지 추가 테스트

--- src/main/java/org/fp024/mvcpractice/controller/HelloController.java
+++ src/main/java/org/fp024/mvcpractice/controller/HelloController.java
@@ -2,12 +2,22 @@ package org.fp024.mvcpractice.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 import org.fp024.mvcpractice.service.HelloService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 @ToString
+@RequestMapping("/sample")
 public class HelloController {
   private final HelloService helloService;
+
+  @GetMapping("/ex1")
+  public void ex1() {
+    log.info("/sample/ex1");
+  }
 }
