f03d26edd81858bdc59ec1e1e5001c209ce5b6bb
fp024 <aopenasus@naver.com>
2026-08-31 15:05:47 +0900

    HikariCP 설정 적용. 그리고 그외 컴파일러 옵션 정리
    -Xlint:all
    자바 컴파일러의 모든 경고(Warning)를 활성화.
    
    -Xlint:-processing
    Lombok 등 어노테이션 프로세서와 관련된 경고(예: 어노테이션 프로세서가 처리하지 못한 미지원 어노테이션 관련 경고)를 비활성화.
    -Xlint:all에 포함된 경고 중 하나를 명시적으로 제외한 것이므로 중복이 아님.
    
    -proc:full
    어노테이션 프로세싱과 컴파일을 모두 수행하도록 강제함. (Java 21부터 기본값 동작이 약간 바뀔 수 있어 컴파일 에러 예방을 위해 흔히 사용됨.)
    
    -Xlint:-path
    bad path element 등 클래스패스에 존재하지 않는 디렉터리나 잘못된 경로 파일 관련 경고를 비활성화함.
    .
    -Xlint:-classfile
    이미 컴파일된 클래스 파일(classfile)을 참조하는 도중 발생하는 종속성 경고를 비활성화함.

--- src/main/java/org/fp024/mvcpractice/config/RootConfig.java
+++ src/main/java/org/fp024/mvcpractice/config/RootConfig.java
@@ -1,11 +1,17 @@
 package org.fp024.mvcpractice.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 
 @Configuration
+@PropertySource({"classpath:database.properties"})
 @ComponentScan(
     basePackages = {"org.fp024.mvcpractice"},
     excludeFilters = {
@@ -17,7 +23,27 @@ import org.springframework.stereotype.Controller;
           classes = {ServletConfig.class})
     })
 public class RootConfig {
-  /*
-   Root Context: defines shared resources visible to all other web components
-  */
+
+  @Bean(destroyMethod = "close")
+  HikariDataSource dataSource(HikariConfig hikariConfig) {
+    return new HikariDataSource(hikariConfig);
+  }
+
+  @Bean
+  HikariConfig hikariConfig(
+      @Value("${jdbc.driver}") String driverClassName,
+      @Value("${jdbc.url}") String url,
+      @Value("${jdbc.username}") String userName,
+      @Value("${jdbc.password}") String password) {
+    HikariConfig hikariConfig = new HikariConfig();
+    hikariConfig.setDriverClassName(driverClassName);
+    hikariConfig.setJdbcUrl(url);
+    hikariConfig.setUsername(userName);
+    hikariConfig.setPassword(password);
+
+    hikariConfig.setConnectionTimeout(30_000L);
+    hikariConfig.setMinimumIdle(2);
+
+    return hikariConfig;
+  }
 }
