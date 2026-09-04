c78642b3016dfe9446f78a4c8376b2db161bf98a
fp024 <aopenasus@naver.com>
2026-09-04 15:15:37 +0900

    `@Transactional` 동작 확인 - 05. 스프링 AOP와 트랜젝션 - 완료

--- src/main/java/org/fp024/mvcpractice/service/TestService.java
+++ src/main/java/org/fp024/mvcpractice/service/TestService.java
@@ -4,14 +4,17 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.fp024.mvcpractice.mapper.TestMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class TestService {
   private final TestMapper testMapper;
 
   public void insertAll(String str) {
+
     int resultA = testMapper.insertA(str);
 
     log.info("insertA {}", resultA);
--- src/test/java/org/fp024/mvcpractice/service/TestServiceTests.java
+++ src/test/java/org/fp024/mvcpractice/service/TestServiceTests.java
@@ -3,6 +3,7 @@ package org.fp024.mvcpractice.service;
 import lombok.extern.slf4j.Slf4j;
 import org.fp024.mvcpractice.config.RootConfig;
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
@@ -14,9 +15,10 @@ class TestServiceTests {
 
   @Test
   void testInsertAll() {
-    String str =
-        "Alice was Beginning to get very tired of sitting by her sister on the bank, and of having"
-            + " nothing todo";
+
+    log.info("is proxy? {}", AopUtils.isAopProxy(testService));
+
+    String str = "Alice was beginning";
 
     testService.insertAll(str);
   }
