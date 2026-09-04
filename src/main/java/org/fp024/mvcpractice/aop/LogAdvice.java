41ae858c00fb503222132a55fe13b5641fc25349
fp024 <aopenasus@naver.com>
2026-09-04 13:57:30 +0900

    JoinPoint 파라미터로 전달 인자, 대상 객체 확인

--- src/main/java/org/fp024/mvcpractice/aop/LogAdvice.java
+++ src/main/java/org/fp024/mvcpractice/aop/LogAdvice.java
@@ -1,6 +1,8 @@
 package org.fp024.mvcpractice.aop;
 
+import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -11,9 +13,18 @@ import org.springframework.stereotype.Component;
 public class LogAdvice {
 
   @Before("execution(* org.fp024.mvcpractice.service.*.*(..))")
-  public void logParams() {
+  public void logParams(JoinPoint jp) {
     log.info("----------");
     log.info("logParams");
+
+    Object[] params = jp.getArgs();
+
+    log.info(Arrays.toString(params));
+
+    Object target = jp.getTarget();
+
+    log.info("{}", target);
+
     log.info("----------");
   }
 }
