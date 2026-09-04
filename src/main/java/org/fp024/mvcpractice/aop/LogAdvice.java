c4c7a23b35cbab8c4c6d3531154bd45e7778dc4f
fp024 <aopenasus@naver.com>
2026-09-04 14:06:53 +0900

    `@Around`의 사용 예제
    `@Before` 보다 먼저 실행된다.
    인터셉터 같이 동작하는듯...😅

--- src/main/java/org/fp024/mvcpractice/aop/LogAdvice.java
+++ src/main/java/org/fp024/mvcpractice/aop/LogAdvice.java
@@ -3,6 +3,8 @@ package org.fp024.mvcpractice.aop;
 import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -26,5 +28,22 @@ public class LogAdvice {
     log.info("{}", target);
 
     log.info("----------");
+  }
+
+  @Around("execution(* org.fp024.mvcpractice.service.*.*(..))")
+  public Object logTime(ProceedingJoinPoint pjp) throws Throwable {
+    log.info("----------");
+    log.info("logTimes");
+
+    long start = System.currentTimeMillis();
+
+    Object result = pjp.proceed();
+
+    long end = System.currentTimeMillis();
+
+    log.info("----------");
+    log.info("TIME: {}", (end - start));
+
+    return result;
   }
 }
