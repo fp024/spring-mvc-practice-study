18e48a8c0c1c3fa3c4e9ea009cd3c7d5903f4fb9
fp024 <aopenasus@naver.com>
2026-09-07 03:07:03 +0900

    게시물 조회 기본 틀
    근데... 컨트롤러에서 RequestMapping 주소 쓸때..
    
    하위 메서드에 /를 붙여주는게
    나을 것 같기도하다.
    
    안붙여도 잘 처리가 되긴하지만...

--- src/main/java/org/fp024/mvcpractice/controller/BoardController.java
+++ src/main/java/org/fp024/mvcpractice/controller/BoardController.java
@@ -4,31 +4,40 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/board/")
+@RequestMapping("/board")
 @Slf4j
 @RequiredArgsConstructor
 public class BoardController {
 
-  @GetMapping("list")
+  @GetMapping("/list")
   public void list() {
     log.info("------------------------------");
     log.info("board list");
   }
 
-  @GetMapping("register")
+  @GetMapping("/register")
   public void register() {
     log.info("------------------------------");
     log.info("board register");
   }
 
-  @PostMapping("register")
+  @PostMapping("/register")
   public String registerPost() {
     log.info("------------------------------");
     log.info("board register post");
     return "redirect:/board/list";
+  }
+
+  @GetMapping("/read/{bno}")
+  public String read(@PathVariable("bno") Long bno) {
+    log.info("------------------------------");
+    log.info("board read");
+
+    return "/board/read";
   }
 }
